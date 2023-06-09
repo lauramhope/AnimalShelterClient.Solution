3903ac8d134ed5b7ce2aabb49ed7e30eb9cbe814
Laura Hope <ssteamlaura@gmail.com>
2023-06-09 14:15:26 -0700

    Add .gitignore to project

---
+++ AnimalShelterClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AnimalShelterClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
